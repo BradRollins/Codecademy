7d29704a45cad5487e9069bf11b828e91b5aef04
BradRollins <48097311+BradRollins@users.noreply.github.com>
2019-07-17 15:39:24 -0500

    Add files via upload

---
+++ Getting Started with JavaScript - Section 3 Projects/Race Day/Race Day.js
@@ -0,0 +1,20 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+let earlyRegister = '25';
+if (earlyRegister) {
+    console.log('Registered early.');
+} else {
+    console.log('Did not register early');
+}
+let runnerAge = 18;
+
+/*if(runnerAge>18 && earlyRegister) {
+  raceNumber += 1000;
+}*/
+
+if (runnerAge > 18 && earlyRegister) {
+    console.log(`You will race at 9:30am and your racenumber is ${raceNumber}`);
+} else if (runnerAge > 18 && !earlyRegister) {
+    console.log(`You will race at 11:00am and your racenumber is ${raceNumber}`);
+} else if (runnerAge < 18) {
+    console.log(`You will race at 12:30pm and your racenumber is ${raceNumber}`);
+} else { console.log('Please see registration desk.') }
